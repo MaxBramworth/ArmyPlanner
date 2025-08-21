cf2ef9eeaed0b580ce7b4cbc98d31837ba052867
MaxBramworth <94107143+MaxBramworth@users.noreply.github.com>
2025-08-21 09:26:18 +0100

    tabs are no longer in scope so I'm using a simple save/load instead, setup ui for that

--- ArmyPlanner/MainWindowViewModel.cs
+++ ArmyPlanner/MainWindowViewModel.cs
@@ -8,16 +8,72 @@ using System.Windows.Controls;
 
 namespace ArmyPlanner
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BindableBase
     {
-        public ObservableCollection<UserControl> Armies { get; set; } = new();
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public int SelectedAllUnitsIndex
+        {
+            get => _selectedAllunitsIndex;
+            set => SetProperty(ref _selectedAllunitsIndex, value);
+        }
+
+        public int SelectedArmyIndex
+        {
+            get => _selectedArmyIndex;
+            set => SetProperty(ref _selectedArmyIndex, value);
+        }
+
+        public string PointsTotal
+        {
+            get => _pointsTotal;
+            set => SetProperty(ref _pointsTotal, value);
+        }
+
+        public ObservableCollection<ArmyUnit> AvailableArmyUnits { get; set; } = [];
+
+        public ObservableCollection<ArmyUnit> CurrentArmy { get; set; } = [];
 
         public MainWindowViewModel()
         {
-            foreach (var army in FileReader.GetAllSavedArmies())
+            foreach (var unit in FileReader.GetAllUnits())
             {
-                Armies.Add(new ArmyView());
+                AvailableArmyUnits.Add(unit);
             }
+
+            CurrentArmy.CollectionChanged += (_, _) => RecalculatePointsTotal();
         }
+
+        public RelayCommand AddToArmyCommand => new(AddToArmy);
+        public RelayCommand RemoveFromArmyCommand => new(RemoveFromArmy);
+
+        void AddToArmy()
+        {
+            CurrentArmy.Add(AvailableArmyUnits[SelectedAllUnitsIndex]);
+        }
+
+        void RemoveFromArmy()
+        {
+            CurrentArmy.RemoveAt(SelectedArmyIndex);
+        }
+
+        void RecalculatePointsTotal()
+        {
+            int points = 0;
+
+            foreach (var unit in CurrentArmy)
+            {
+                points += unit.PointsCost;
+            }
+
+            PointsTotal = $"Total: {points}";
+        }
+
+        private string _title = "Army", _pointsTotal = "Total: 0";
+        private int _selectedAllunitsIndex, _selectedArmyIndex;
     }
 }
